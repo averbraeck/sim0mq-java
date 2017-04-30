0bb40ae5e8b987ace553583f39304d9ca44877b4
averbraeck <averbraeck@62a16a09-b751-4741-9255-0c391080f8a5>
2017-04-30 22:35:30 +0000

    Sim0MQ version 0.02.00. Units and DisplayUnits adapted to the changes in DJUNITS version 3.00.01. Two bytes used for the Money type as 255 is too low, and cannot store the standard currency id codes.

---
+++ release/0.02.00/sim0mq/src/main/java/org/sim0mq/federationmanager/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * The FederationManager is an example class that sends messages to FederateStarters to start model components, loggers, data
+ * collectors, etc. It can start a federation as soon as all required model components are on-line. It might kill model
+ * components that take too long to finish.
+ * <p>
+ * Copyright (c) 2016-2017 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://sim0mq.org/docs/current/license.html">Sim0MQ License</a>.
+ * </p>
+ * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+ * initial version Mar 1, 2017 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+package org.sim0mq.federationmanager;
