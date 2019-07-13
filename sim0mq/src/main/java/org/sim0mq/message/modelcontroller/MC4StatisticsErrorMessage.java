87401737db124f4ead2c8a2bda5aed2f207d48cb
Alexander Verbraeck <averbraeck@users.noreply.github.com>
2019-07-13 17:56:30 +0000

    MC messages plus test coverage built.

--- sim0mq/src/main/java/org/sim0mq/message/modelcontroller/MC4StatisticsErrorMessage.java
+++ sim0mq/src/main/java/org/sim0mq/message/modelcontroller/MC4StatisticsErrorMessage.java
@@ -4,31 +4,31 @@ import org.djutils.exceptions.Throw;
 import org.djutils.serialization.SerializationException;
 import org.sim0mq.Sim0MQException;
 import org.sim0mq.message.MessageStatus;
-import org.sim0mq.message.Sim0MQReply;
+import org.sim0mq.message.Sim0MQMessage;
 import org.sim0mq.message.SimulationMessage;
 
 /**
- * StatusMessage, MC.1. The Model sends this message as a response to RequestStatus messages sent by the Federate Starter or the
- * Federation Manager.
+ * StatisticsError, MC.4. The Model sends this message as a response to RequestStatistics messages sent by the Federation
+ * Manager, when e.g., the requested variable could not be found.
  * <p>
  * Copyright (c) 2016-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="http://sim0mq.org/docs/current/license.html">Sim0MQ License</a>.
  * </p>
  * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  */
-public class MC1StatusMessage extends Sim0MQReply
+public class MC4StatisticsErrorMessage extends Sim0MQMessage
 {
-    /** A string that refers to the model status. Four options: "started", "running", "ended", "error". */
-    private final String status;
+    /** The name of the output variable whose value is requested. That should match with the name in the model. */
+    private final String variableName;
 
-    /** Optional. If there is an error, the error message is sent as well. Otherwise this field is an empty string. */
+    /** Error retrieving the model output value. */
     private final String error;
 
     /** the unique message id. */
-    private static final String MESSAGETYPE = "MC.1";
+    private static final String MESSAGETYPE = "MC.4";
 
     /** */
-    private static final long serialVersionUID = 20170422L;
+    private static final long serialVersionUID = 20190712;
 
     /**
      * @param simulationRunId the Simulation run ids can be provided in different types. Examples are two 64-bit longs
@@ -39,39 +39,34 @@ public class MC1StatusMessage extends Sim0MQReply
      *            error can be sent if we receive a message not meant for us).
      * @param messageId The unique message number is meant to confirm with a callback that the message has been received
      *            correctly. The number is unique for the sender, so not globally within the federation.
-     * @param uniqueId Id to identify the callback to the message.
-     * @param status A string that refers to the model status. Four options: "started", "running", "ended", "error".
-     * @param error Optional. If there is an error, the error message is sent as well. Otherwise this field is an empty string.
+     * @param variableName The name of the output variable whose value is requested. That should match with the name in the
+     *            model.
+     * @param error String; Error retrieving the model output value.
      * @throws Sim0MQException on unknown data type
      * @throws NullPointerException when one of the parameters is null
      */
-    public MC1StatusMessage(final Object simulationRunId, final Object senderId, final Object receiverId, final long messageId,
-            final long uniqueId, final String status, final String error) throws Sim0MQException, NullPointerException
+    public MC4StatisticsErrorMessage(final Object simulationRunId, final Object senderId, final Object receiverId,
+            final long messageId, final String variableName, final String error) throws Sim0MQException, NullPointerException
     {
-        super(simulationRunId, senderId, receiverId, MESSAGETYPE, messageId, MessageStatus.NEW, uniqueId);
-        Throw.whenNull(status, "status cannot be null");
-        Throw.whenNull(error, "error cannot be null");
-
-        Throw.when(status.isEmpty(), Sim0MQException.class, "status cannot be empty");
-        Throw.when(!status.equals("started") && !status.equals("running") && !status.equals("ended") && !status.equals("error"),
-                Sim0MQException.class, "status should be one of 'started', 'running', 'ended', 'error'");
-
-        this.status = status;
+        super(simulationRunId, senderId, receiverId, MESSAGETYPE, messageId, MessageStatus.NEW);
+        Throw.whenNull(variableName, "variableName cannot be null");
+        Throw.whenNull(error, "errorValue cannot be null");
+        this.variableName = variableName;
         this.error = error;
     }
 
     /**
-     * @return status
+     * @return variableName
      */
-    public final String getStatus()
+    public String getVariableName()
     {
-        return this.status;
+        return this.variableName;
     }
 
     /**
      * @return error
      */
-    public final String getError()
+    public String getError()
     {
         return this.error;
     }
@@ -88,7 +83,7 @@ public class MC1StatusMessage extends Sim0MQReply
     @Override
     public short getNumberOfPayloadFields()
     {
-        return 3;
+        return 2;
     }
 
     /** {@inheritDoc} */
@@ -96,7 +91,7 @@ public class MC1StatusMessage extends Sim0MQReply
     public Object[] createObjectArray()
     {
         return new Object[] {getMagicNumber(), getSimulationRunId(), getSenderId(), getReceiverId(), getMessageTypeId(),
-                getMessageId(), getMessageStatus(), getNumberOfPayloadFields(), getReplyToId(), this.status, this.error};
+                getMessageId(), getMessageStatus(), getNumberOfPayloadFields(), this.variableName, this.error};
     }
 
     /** {@inheritDoc} */
@@ -104,7 +99,7 @@ public class MC1StatusMessage extends Sim0MQReply
     public byte[] createByteArray() throws Sim0MQException, SerializationException
     {
         return SimulationMessage.encodeUTF8(getSimulationRunId(), getSenderId(), getReceiverId(), getMessageTypeId(),
-                getMessageId(), getMessageStatus(), getReplyToId(), this.status, this.error);
+                getMessageId(), getMessageStatus(), this.variableName, this.error);
     }
 
     /**
@@ -114,11 +109,12 @@ public class MC1StatusMessage extends Sim0MQReply
      * @return a Sim0MQ message
      * @throws Sim0MQException when number of fields is not correct
      */
-    public static MC1StatusMessage createMessage(final Object[] fields, final Object intendedReceiverId) throws Sim0MQException
+    public static MC4StatisticsErrorMessage createMessage(final Object[] fields, final Object intendedReceiverId)
+            throws Sim0MQException
     {
-        check(fields, 3, MESSAGETYPE, intendedReceiverId);
-        return new MC1StatusMessage(fields[1], fields[2], fields[3], ((Long) fields[5]).longValue(),
-                ((Long) fields[8]).longValue(), fields[9].toString(), fields[10].toString());
+        check(fields, 2, MESSAGETYPE, intendedReceiverId);
+        return new MC4StatisticsErrorMessage(fields[1], fields[2], fields[3], ((Long) fields[5]).longValue(),
+                fields[8].toString(), fields[9].toString());
     }
 
     /**
@@ -131,12 +127,12 @@ public class MC1StatusMessage extends Sim0MQReply
      * </p>
      * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
      */
-    public static class Builder extends Sim0MQReply.Builder<MC1StatusMessage.Builder>
+    public static class Builder extends Sim0MQMessage.Builder<MC4StatisticsErrorMessage.Builder>
     {
-        /** A string that refers to the model status. Four options: "started", "running", "ended", "error". */
-        private String status;
+        /** The name of the output variable whose value is requested. That should match with the name in the model. */
+        private String variableName;
 
-        /** Optional. If there is an error, the error message is sent as well. Otherwise this field is an empty string. */
+        /** Error message retrieving the output value from the model. */
         private String error;
 
         /**
@@ -148,12 +144,12 @@ public class MC1StatusMessage extends Sim0MQReply
         }
 
         /**
-         * @param newStatus set status
+         * @param newVariableName set variable name
          * @return the original object for chaining
          */
-        public final Builder setStatus(final String newStatus)
+        public final Builder setVariableName(final String newVariableName)
         {
-            this.status = newStatus;
+            this.variableName = newVariableName;
             return this;
         }
 
@@ -169,10 +165,10 @@ public class MC1StatusMessage extends Sim0MQReply
 
         /** {@inheritDoc} */
         @Override
-        public MC1StatusMessage build() throws Sim0MQException, NullPointerException
+        public MC4StatisticsErrorMessage build() throws Sim0MQException, NullPointerException
         {
-            return new MC1StatusMessage(this.simulationRunId, this.senderId, this.receiverId, this.messageId, this.replyToId,
-                    this.status, this.error);
+            return new MC4StatisticsErrorMessage(this.simulationRunId, this.senderId, this.receiverId, this.messageId,
+                    this.variableName, this.error);
         }
 
     }
