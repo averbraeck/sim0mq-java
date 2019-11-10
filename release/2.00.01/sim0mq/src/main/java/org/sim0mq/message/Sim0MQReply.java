cab9d6e3a8c9100522aa1889ed38daf41ca18e56
averbraeck <averbraeck@62a16a09-b751-4741-9255-0c391080f8a5>
2019-11-10 01:24:01 +0000

    deployed sim0mq with javadoc repair for java-13

---
+++ release/2.00.01/sim0mq/src/main/java/org/sim0mq/message/Sim0MQReply.java
@@ -0,0 +1,139 @@
+package org.sim0mq.message;
+
+import org.djutils.exceptions.Throw;
+import org.sim0mq.Sim0MQException;
+
+/**
+ * The abstract body of a reply message with the first fields of every Sim0MQ reply message.
+ * <p>
+ * Copyright (c) 2016-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://sim0mq.org/docs/current/license.html">Sim0MQ License</a>.
+ * </p>
+ * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+ * initial version Apr 22, 2017 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public abstract class Sim0MQReply extends Sim0MQMessage
+{
+    /** */
+    private static final long serialVersionUID = 20170422L;
+
+    /** The unique message id (Frame 5) of the sender for which this is the reply. */
+    private final Object replyToId;
+
+    /**
+     * Encode the object array into a message.
+     * @param bigEndian boolean; Indicates whether this message using little endian or big endian encoding. Big endian is
+     *            encoded as true, and little endian as false.
+     * @param federationId the federation id can be coded using different types. Examples are two 64-bit longs indicating a
+     *            UUID, or a String with a UUID number, a String with meaningful identification, or a short or an int with a
+     *            simulation run number.
+     * @param senderId The sender id can be used to send back a message to the sender at some later time.
+     * @param receiverId The receiver id can be used to check whether the message is meant for us, or should be discarded (or an
+     *            error can be sent if we receive a message not meant for us).
+     * @param messageTypeId Message type ids can be defined per type of simulation, and can be provided in different types.
+     *            Examples are a String with a meaningful identification, or a short or an int with a message type number.
+     * @param messageId The unique message number is meant to confirm with a callback that the message has been received
+     *            correctly. The number is unique for the sender, so not globally within the federation.
+     * @param payload Object[]; Payload as an Object array
+     * @throws Sim0MQException on unknown data type
+     * @throws NullPointerException when one of the parameters is null
+     */
+    public Sim0MQReply(final boolean bigEndian, final Object federationId, final Object senderId, final Object receiverId,
+            final Object messageTypeId, final Object messageId, final Object[] payload)
+            throws Sim0MQException, NullPointerException
+    {
+        super(bigEndian, federationId, senderId, receiverId, messageTypeId, messageId, payload);
+        Throw.when(payload.length < 1, Sim0MQException.class, "payload for a reply should start with the replyToId");
+        this.replyToId = payload[0];
+    }
+
+    /**
+     * Encode the object array into a message. <br>
+     * 0 = magic number, equal to the String "SIM##" where ## stands for the version number of the protocol.<br>
+     * 1 = endianness, boolean indicating the endianness for the message. True is Big Endian, false is Little Endian.<br>
+     * 2 = federation id, could be String, int, Object, ...<br>
+     * 3 = sender id, could be String, int, Object, ...<br>
+     * 4 = receiver id, could be String, int, Object, ...<br>
+     * 5 = message type id, could be String, int, Object, ...<br>
+     * 6 = message id, could be long, Object, String, ....<br>
+     * 7 = number of fields that follow, as a Number (byte, short, int, long).<br>
+     * 8-n = payload, where the number of fields was defined by message[7]. The first payload field should be the replyToId.
+     * @param objectArray Object[]; Full message object array
+     * @param expectedNumberOfPayloadFields int; the expected number of fields in the message (field 8 and further). The payload
+     *            fields should include the replyToId, so the number should be 1 or higher.
+     * @param expectedMessageTypeId the expected message type id
+     * @throws Sim0MQException on unknown data type
+     * @throws NullPointerException when one of the parameters is null
+     */
+    public Sim0MQReply(final Object[] objectArray, final int expectedNumberOfPayloadFields, final Object expectedMessageTypeId)
+            throws Sim0MQException, NullPointerException
+    {
+        super(objectArray, expectedNumberOfPayloadFields, expectedMessageTypeId);
+        Throw.when(objectArray.length < 9, Sim0MQException.class, "payload for a reply should start with the replyToId");
+        this.replyToId = objectArray[8];
+    }
+
+    /**
+     * @return replyToId
+     */
+    public final Object getReplyToId()
+    {
+        return this.replyToId;
+    }
+
+    /**
+     * Builder for the Sim0MQReply. Can string setters together, and call build() at the end to build the actual message.
+     * <p>
+     * Copyright (c) 2016-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved.
+     * <br>
+     * BSD-style license. See <a href="http://sim0mq.org/docs/current/license.html">Sim0MQ License</a>.
+     * </p>
+     * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+     * initial version Apr 22, 2017 <br>
+     * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+     * @param <B> the actual inherited builder for the return types.
+     */
+    public abstract static class Builder<B extends Sim0MQMessage.Builder<B>> extends Sim0MQMessage.Builder<B>
+    {
+        /** The unique message id (Frame 5) of the sender for which this is the reply. */
+        @SuppressWarnings("checkstyle:visibilitymodifier")
+        protected Object replyToId;
+
+        /**
+         * Empty constructor.
+         */
+        public Builder()
+        {
+            // nothing to do.
+        }
+
+        /**
+         * @param newReplyToId set replyToId
+         * @return the original object for chaining
+         */
+        @SuppressWarnings("unchecked")
+        public final B setReplyToId(final Object newReplyToId)
+        {
+            this.replyToId = newReplyToId;
+            return (B) this;
+        }
+
+        /**
+         * @param message set replyToId and receiver based on the message to which this is a reply
+         * @return the original object for chaining
+         */
+        @SuppressWarnings("unchecked")
+        public final B setReplyTo(final Sim0MQMessage message)
+        {
+            this.replyToId = message.getMessageId();
+            this.receiverId = message.getSenderId();
+            return (B) this;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public abstract Sim0MQReply build() throws Sim0MQException, NullPointerException;
+
+    }
+}
---
+++ release/2.00.01/sim0mq/src/main/java/org/sim0mq/message/types/NumberDuration.java
@@ -0,0 +1,217 @@
+package org.sim0mq.message.types;
+
+import java.io.Serializable;
+
+import org.djunits.value.vdouble.scalar.Duration;
+import org.djunits.value.vfloat.scalar.FloatDuration;
+import org.sim0mq.Sim0MQException;
+
+/**
+ * Wrapper for a Number or float/double with Unit of type Duration. Store it internally as a Number <b>or</b> as a DoubleScalar,
+ * <b>or</b> as a FloatScalar and have methods to retrieve it in different ways.
+ * <p>
+ * Copyright (c) 2016-2019 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * BSD-style license. See <a href="http://sim0mq.org/docs/current/license.html">Sim0MQ License</a>.
+ * </p>
+ * $LastChangedDate: 2015-07-24 02:58:59 +0200 (Fri, 24 Jul 2015) $, @version $Revision: 1147 $, by $Author: averbraeck $,
+ * initial version Apr 24, 2017 <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class NumberDuration extends Number implements Serializable
+{
+    /** */
+    private static final long serialVersionUID = 20170424L;
+
+    /** Number - any of the number types. */
+    private final Number duration;
+
+    /** DoubleScalar of type Duration. */
+    private final Duration doubleScalar;
+
+    /** FloatScalar of type FloatDuration. */
+    private final FloatDuration floatScalar;
+
+    /**
+     * Create a duration from a Number.
+     * @param duration the duration as a Number.
+     */
+    public NumberDuration(final Number duration)
+    {
+        this.duration = duration;
+        this.doubleScalar = null;
+        this.floatScalar = null;
+    }
+
+    /**
+     * Create a duration from a DoubleScalar Duration type.
+     * @param duration the duration as a DoubleScalar Duration.
+     */
+    public NumberDuration(final Duration duration)
+    {
+        this.duration = null;
+        this.doubleScalar = duration;
+        this.floatScalar = null;
+    }
+
+    /**
+     * Create a duration from a FloatScalar FloatDuration type.
+     * @param duration the duration as a FloatScalar FloatDuration.
+     */
+    public NumberDuration(final FloatDuration duration)
+    {
+        this.duration = null;
+        this.doubleScalar = null;
+        this.floatScalar = duration;
+    }
+
+    /**
+     * Instantiate a NumberDuration based on a value.
+     * @param value the value that can be Duration, FloatDuration, or Number
+     * @return an instantiation of NumberDuration
+     * @throws Sim0MQException if the value is neither Duration, FloatDuration, nor Number
+     */
+    public static NumberDuration instantiate(final Object value) throws Sim0MQException
+    {
+        if (value instanceof Duration)
+        {
+            return new NumberDuration((Duration) value);
+        }
+        else if (value instanceof FloatDuration)
+        {
+            return new NumberDuration((FloatDuration) value);
+        }
+        else if (value instanceof Number)
+        {
+            return new NumberDuration((Number) value);
+        }
+        else
+        {
+            throw new Sim0MQException("value should be Number, Duration or FloatDuration");
+        }
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    public int intValue()
+    {
+        return this.duration.intValue();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public long longValue()
+    {
+        return this.duration.longValue();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public float floatValue()
+    {
+        return this.duration.floatValue();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public double doubleValue()
+    {
+        return this.duration.doubleValue();
+    }
+
+    /**
+     * Return the NumberDuration as an object, e.g., for serializing.
+     * @return NumberDuration as an object
+     */
+    public Object getObject()
+    {
+        if (this.duration != null)
+        {
+            return this.duration;
+        }
+        else if (this.doubleScalar != null)
+        {
+            return this.doubleScalar;
+        }
+        else if (this.floatScalar != null)
+        {
+            return this.floatScalar;
+        }
+        else
+        {
+            // should never happen
+            throw new RuntimeException("NumberDuration is neither Number, nor Duration, nor FloatDuration");
+        }
+    }
+
+    /**
+     * @return the duration as a Number
+     */
+    public Number getNumber()
+    {
+        if (this.duration != null)
+        {
+            return this.duration;
+        }
+        else if (this.doubleScalar != null)
+        {
+            return this.doubleScalar;
+        }
+        else if (this.floatScalar != null)
+        {
+            return this.floatScalar;
+        }
+        else
+        {
+            // should never happen
+            throw new RuntimeException("NumberDuration is neither Number, nor Duration, nor FloatDuration");
+        }
+    }
+
+    /**
+     * @return the duration as a djunits Duration type
+     */
+    public Duration getDuration()
+    {
+        if (this.duration != null)
+        {
+            return Duration.instantiateSI(this.duration.doubleValue());
+        }
+        else if (this.doubleScalar != null)
+        {
+            return this.doubleScalar;
+        }
+        else if (this.floatScalar != null)
+        {
+            return new Duration(this.floatScalar.getInUnit(), this.floatScalar.getDisplayUnit());
+        }
+        else
+        {
+            // should never happen
+            throw new RuntimeException("NumberDuration is neither Number, nor Duration, nor FloatDuration");
+        }
+    }
+
+    /**
+     * @return the duration as a djunits FloatDuration type
+     */
+    public FloatDuration getFloatDuration()
+    {
+        if (this.duration != null)
+        {
+            return FloatDuration.instantiateSI(this.duration.floatValue());
+        }
+        else if (this.doubleScalar != null)
+        {
+            return new FloatDuration((float) this.doubleScalar.getInUnit(), this.doubleScalar.getDisplayUnit());
+        }
+        else if (this.floatScalar != null)
+        {
+            return this.floatScalar;
+        }
+        else
+        {
+            // should never happen
+            throw new RuntimeException("NumberDuration is neither Number, nor Duration, nor FloatDuration");
+        }
+    }
+}
