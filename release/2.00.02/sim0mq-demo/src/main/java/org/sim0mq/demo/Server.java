5ed6c6a3d8f2687f438aa4fdaabdf026d41e98e6
averbraeck <averbraeck@62a16a09-b751-4741-9255-0c391080f8a5>
2020-02-10 20:23:40 +0000

    deployed sim0mq version 2.00.02

---
+++ release/2.00.02/sim0mq-demo/src/main/java/org/sim0mq/demo/Server.java
@@ -0,0 +1,55 @@
+package org.sim0mq.demo;
+
+import org.djutils.serialization.SerializationException;
+import org.sim0mq.Sim0MQException;
+import org.sim0mq.message.Sim0MQMessage;
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+/**
+ * Server example for JeroMQ / ZeroMQ.
+ * <p>
+ * (c) copyright 2015-2020 <a href="http://www.simulation.tudelft.nl">Delft University of Technology</a>. <br>
+ * BSD-style license. See <a href="http://www.simulation.tudelft.nl/dsol/3.0/license.html">DSOL License</a>. <br>
+ * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @version Oct 21, 2016
+ */
+public final class Server
+{
+    /** */
+    private Server()
+    {
+        // Utility class
+    }
+    
+    /**
+     * @param args command line arguments
+     * @throws Sim0MQException on error
+     * @throws SerializationException on serialization problem
+     */
+    public static void main(final String[] args) throws Sim0MQException, SerializationException
+    {
+        ZContext context = new ZContext(1);
+
+        // Socket to talk to clients
+        ZMQ.Socket responder = context.createSocket(SocketType.REP);
+        responder.bind("tcp://*:5556");
+
+        while (!Thread.currentThread().isInterrupted())
+        {
+            // Wait for next request from the client
+            byte[] request = responder.recv(0);
+            Object[] message = Sim0MQMessage.decode(request).createObjectArray();
+            System.out.println("Received " + Sim0MQMessage.print(message));
+
+            // send a reply
+            Object[] reply = new Object[] { true, -28.2, 77000, "Bangladesh" };
+            responder.send(Sim0MQMessage.encodeUTF8(true, "IDVV14.2", "MC.1", "MM1.4", "TEST.2", 1201L, reply),
+                    0);
+        }
+        responder.close();
+        context.destroy();
+        context.close();
+    }
+}
